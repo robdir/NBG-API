7dc88509a5659beeec22b3a9de46efd0810cf682
mrjln <marjolein_tromp@hotmail.com>
2017-11-22 14:20:47 +0100

    bol.com things

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-const express = require('express')
+var app = require('express')()
+var server = require('http').Server(app)
+var io = require('socket.io')(server)
+var bol = require ('bolcom') ('1673B7353F954E90916BFD4931E48C2A');
+
+server.listen(3002)
+
+bol.catalog.search ({ q: 'Web Developer' }, function (err, data) {
+  if (err) {
+    console.log ('Search failed');
+    console.log (err);
+    return;
+  } //if
+
+  for (var p in data.products) {
+    var product = data.products[p];
+    console.log (product.title + ' - €' + product.offerData.offers[0].price);
+    io.emit('action', {
+            	type: 'UPDATE_BOOKS',
+            	payload: item,
+            })
+  }//for
+}) //bolcatalog
+
+io.on('connection', socket => {
+    console.log('got connection')
+  })
+
+/*const express = require('express')
 
 const PORT = process.env.PORT || 3030
 
@@ -10,4 +38,4 @@ app.get('/', (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Hi Marjo, I'm listening on port ${PORT}`)
-})
+}) */

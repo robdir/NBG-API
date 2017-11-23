74f93d30a01ec29884c3f1915e03980cc9c8512e
mrjln <marjolein_tromp@hotmail.com>
2017-11-23 11:08:50 +0100

    added api get books

--- db/seed.js
+++ db/seed.js
@@ -1,6 +1,4 @@
 const request = require('superagent')
-const user = require('./fixtures/user.json')
-const recipes = require('./fixtures/recipes.json')
 
 const createUrl = (path) => {
   return `${process.env.HOST || `http://localhost:${process.env.PORT || 3030}`}${path}`

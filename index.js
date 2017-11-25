1d5955e35f58ec14d52e11d1c0fc647547a8efad
mrjln <marjolein_tromp@hotmail.com>
2017-11-25 15:39:38 +0100

    retrieved data from springest

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 
-var bol = require ('bolcom') ('1673B7353F954E90916BFD4931E48C2A');
+var bol = require ('bolcom') (process.env.BOL_KEY);
+var https = require('https');
 const express = require('express')
 const cors = require('cors')
-
 const PORT = process.env.PORT || 3030
 
 let app = express()
 var books = []
 
+const SPRINGEST_KEY = process.env.SPRINGEST_KEY
+
 app.use(cors())
 
 
-  bol.catalog.search ({ q: 'Web Developer' }, function (err, data) {
+bol.catalog.search ({ q: 'Web Developer' }, function (err, data) {
     if (err) {
       console.log ('Search failed');
       console.log (err);
@@ -25,10 +27,42 @@ app.use(cors())
     }
   })
 
-
 app.get('/books', (req, res) => {
   res.send(books)
 })
+
+var url = `https://api.springest.nl/trainings.json?api_key=${SPRINGEST_KEY}&query=Webdeveloper`
+
+var req = https.get(url, function(res) {
+  console.log('STATUS: ' + res.statusCode);
+  console.log('HEADERS: ' + JSON.stringify(res.headers));
+
+    var bodyChunks = [];
+     res.on('data', function(chunk) {
+       // You can process streamed parts here...
+       console.log("CHUNK", chunk)
+       bodyChunks.push(chunk);
+     }).on('end', function() {
+       var body = Buffer.concat(bodyChunks);
+       app.get('/courses', (req, res) => {
+         res.send(body)
+       })
+     })
+   });
+
+   req.on('error', function(e) {
+  console.log('ERROR: ' + e.message);
+});
+
+/*https.get(url, function(courses){
+
+  console.log(courses)
+
+  app.get('/courses', (req, res) => {
+    res.send(JSON.stringify(courses))
+  })
+}) */
+
 
 app.listen(PORT, () => {
   console.log(`Hi Marjo, I'm listening on port ${PORT}`)
